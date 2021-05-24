c7f8619cde66888b64b163b139e24545480e6873
terakawaakari <terakawaakari0701@gmail.com>
2021-05-24 10:22:07 +0000

    サイドバーにフォローボタン設置

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,10 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     @other_user = User.find(params[:follower])
     current_user.follow(@other_user)
+    redirect_back(fallback_location: root_path)
   end
-  
+
   def destroy
     @user = current_user.relationships.find(params[:id]).follower
     current_user.unfollow(params[:id])
+    redirect_back(fallback_location: root_path)
   end
 end
