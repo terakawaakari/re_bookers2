6715407d607dcab9d269f9735beba45e56bc8e4a
terakawaakari <terakawaakari0701@gmail.com>
2021-06-03 07:16:58 +0000

    課題7b終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,24 @@ class UsersController < ApplicationController
     @books = @user.books.page(params[:page]).reverse_order
     @user_id = current_user
     @book = Book.new
+    @today_books = Book.where(user_id: @user).where(created_at: Time.zone.now.all_day)
+    @yesterday_books = Book.where(user_id: @user).where(created_at: 1.day.ago.all_day)
+
+    today  = Time.current.at_end_of_day
+    lastweek  = (today - 6.day).at_beginning_of_day
+    lastweek_end = (today - 6.day).at_end_of_day
+    twoweek_ago = (lastweek_end - 6.day).at_beginning_of_day
+
+    @week_books = Book.where(user_id: @user).where(created_at: lastweek...today)
+    @lasteweek_books = Book.where(user_id: @user).where(created_at: lastweek_end...twoweek_ago)
+
+    begin
+      @day_ratio = "#{@today_books}".to_i / "#{@yesterday_books}".to_i * 100
+      @week_ratio = "#{@week_books}".to_i / "#{@lastweek_books}".to_i * 100
+    rescue ZeroDivisionError
+      @day_ratio = 0
+      @week_ratio = 0
+    end
   end
 
   def edit
