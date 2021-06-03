1be5d735f4e2bc14b4336b43102b7a9871b2cb07
terakawaakari <terakawaakari0701@gmail.com>
2021-06-03 13:14:36 +0000

    グループ編集機能の作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
 
+ before_action :ensure_current_user, {only: [:edit, :update]}
+
  def index
    @book = Book.new
    @groups = Group.all
@@ -23,9 +25,25 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render "edit"
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, :introduction, :image)
+  end
+
+  def ensure_current_user
+    group = Group.find(params[:id])
+    if group.owner_id != current_user.id
+      redirect_to groups_path, alert: "権限がありません"
+    end
   end
 
 end
